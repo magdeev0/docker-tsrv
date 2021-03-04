8b8f16bc618a9e937fe154f60ac684d966920dac
magdeev0 <bulat@magdeev.net>
2021-03-04 09:32:07 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,5 @@
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
     public static void main(String[] args) {
-        try (
-                ServerSocket serverSocket = new ServerSocket(9999);
-        ) {
-            while (true) {
-                try (
-                        Socket socket = serverSocket.accept();
-                ) {
-                    OutputStream outputStream = socket.getOutputStream();
-                    String message = "Hello world";
 
-                    outputStream.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: text/plain\r\n" +
-                                    "Content-Length: " + message.length() + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n" +
-                                    message
-                    ).getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
